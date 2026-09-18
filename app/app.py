e8138dccf51c4c855e171e169052907d8f7b893b
Rémy Cspl <45265766+Herowynn@users.noreply.github.com>
2026-09-18 12:10:40 +0200

    feat: page expérience avec items experience dans liste exterieure (sans style)

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from data.experiences import experiences
 
 app = Flask(__name__)
 
@@ -7,8 +8,8 @@ def hello_world():
     return render_template("index.html")
 
 @app.route("/experiences")
-def experiences():
-    return render_template("experiences.html")
+def experiences_page():
+    return render_template("experiences.html", experiences=experiences)
 
 @app.route("/projects")
 def projects():
---
+++ app/data/experiences.py
@@ -0,0 +1,75 @@
+experiences = [
+    {
+        "poste": "Préparateur de commandes (emploi intérimaire)",
+        "entreprise": {
+            "nom": "Manpower, Vertbaudet",
+            "lieu": "Marquette-lez-Lille"
+        },
+        "date": "Janvier 2026 - Avril 2026",
+        "duree": "4 mois",
+        "description": [
+                "Prélèvement de produits dans les stocks selon les besoins de la commande, organisation des articles en vue de l'emballage"
+            ],
+        "technologies": [], 
+        "competences": ["Rigueur", "Rapidité d'exécution", "Travail en équipe"]
+    },
+    {
+        "poste": "Agent de quai (emploi intérimaire)",
+        "entreprise": {
+            "nom": "ManPower, UPS",
+            "lieu": "Lesquin"
+        },
+        "date": "Août 2025 - Décembre 2025",
+        "duree": "5 mois",
+        "description": [
+                "Collecte et réorientation des colis avec erreurs de tri en fin de ligne de traitement, remise en circuit pour assurer la bonne expédition."
+            ],
+        "technologies": [],
+        "competences": ["Sens du détail", "Autonomie", "Contribution à la lfuidité logistique", "Travail en équipe"]
+    },
+    {
+        "poste": "Professeur particulier de Mathématiques",
+        "entreprise": {
+            "nom": "Acadomia",
+            "lieu": "Lille et région"
+        },
+        "date": "Mars 2025 - Juillet 2025",
+        "duree": "4 mois",
+        "description": [
+                "Accompagnement personnalisé d'élèves de niveau collège, préparation aux examens (Brevet) et création de supports pédagogiques."
+            ],
+        "technologies": [],
+        "competences": ["Pédagogie", "Rigueur", "Adaptablité", "Communication", "Gestion du temps"]
+    },
+    {
+        "poste": "Stage - Développeur Web",
+        "entreprise": {
+            "nom": "LES VRAIS",
+            "lieu": "Anvers, Belgique"
+        },
+        "date": "Février 2024 - Août 2024",
+        "duree": "6 mois",
+        "description": [
+                "Refonte complète du site web vitrine (optimisation responsive et UX), et ajout de fonctionnalités,",
+                "Amélioration du SEO et réduction des temps de chargement,",
+                "Développement et déploiement d'un outil de gestion et de création de factures (utilisateurs, produits, clients...)."
+            ],
+        "technologies": ["Wordpress", "ASP.Net Core", "C#", "SQL", "MYSQL", "PostgreSQL", "JavaScript", "HTML", "CSHTML", "CSS", "PHP"],
+        "competences": ["Adaptabilité", "Autonomie", "Sens du détail"]
+    },
+    {
+        "poste": "Stage - Développeur Web",
+        "entreprise": {
+            "nom": "IDKIDS",
+            "lieu": "Roubaix"
+        },
+        "date": "Mai 2026 - Août 2026",
+        "duree": "4 mois",
+        "description": [
+                "Correction de bugs et ajout de fonctionnalités au sein de l'outil de monitoring interne de l'entreprise,",
+                "Création et industrialisation d'un système complet de notification, de l'implémentation de la base de données à celle des fonctionnalités et du visuel.",
+            ],
+        "technologies": ["ASP.Net", "SQL Server", "C#", "JavaScript", "CSHTML", "CSS"],
+        "competences": ["Adaptabilité", "Autonomie", "Rigueur", "Travail en équipe"]
+    },
+]
