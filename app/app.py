0a04fbc4ab8254a74407ea3bade33e6df9e74b42
Rémy Cspl <45265766+Herowynn@users.noreply.github.com>
2026-09-18 11:31:44 +0200

    feat: pages placeholder pour les différentes premières sections

---
+++ app/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template("index.html")
+
+@app.route("/experiences")
+def experiences():
+    return render_template("experiences.html")
+
+@app.route("/projects")
+def projects():
+    return render_template("projets.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
